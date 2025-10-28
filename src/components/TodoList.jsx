e64e4219d58c65ec6a1d60efed06a4747f1b6615
Tommy Su <81478928+helmet323@users.noreply.github.com>
2025-10-28 10:55:08 -0400

    update tasklist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -16,10 +16,32 @@ export default function TodoList() {
   const [taskPriority, setTaskPriority] = useState("");
   const taskInputRef = useRef(null);
 
+  const [edittingText, setEdittingText] = useState("");
+  const [edittingPriority, setEdittingPriority] = useState("");
+  const [edittingMode, setEdittingMode] = useState(null);
+
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [checked, setChecked] = useState(false);
+
+  const [error, setError] = useState("");
+
+  // exists => true
+  const checkPriority = (priority) => {
+    const exists = tasks.filter((e) => e.priority === priority);
+    return exists.length === 1;
+  };
+
   const addTask = (e) => {
     e.preventDefault();
     const priority = parseInt(taskPriority);
     if (!taskText || isNaN(priority) || priority <= 0) return;
+
+    const checkPrio = checkPriority(priority);
+    if (checkPrio) {
+      setError("Priority already exists");
+      return;
+    }
+    setError("");
 
     const newTask = { text: taskText, priority };
     const newTasks = [...tasks, newTask].sort(
@@ -53,6 +75,45 @@ export default function TodoList() {
     return missing.join(", ");
   };
 
+  const goIntoEditting = (index, task) => {
+    setEdittingMode(index);
+    setEdittingPriority(task.priority);
+    setEdittingText(task.text);
+  };
+
+  const saveEdit = (index) => {
+    const priority = parseInt(edittingPriority);
+    if (!edittingText || isNaN(priority) || priority <= 0) return;
+
+    const checkPrio = checkPriority(priority);
+    if (checkPrio) {
+      setError("Priority already exists");
+      return;
+    }
+
+    setError("");
+
+    const newTask = { text: edittingText, priority };
+    const taskList = [...tasks];
+    taskList[index] = newTask;
+    const sortedTasks = taskList.sort((a, b) => a.priority - b.priority);
+    setTasks(sortedTasks);
+
+    setEdittingMode(null);
+    setEdittingPriority("");
+    setEdittingText("");
+  };
+
+  const handleComplete = (index) => {
+    const newCompleteList = [...completedTasks, { text: tasks[index].text }];
+    setCompletedTasks(newCompleteList);
+
+    const taskList = tasks.filter((e, i) => i != index);
+    setTasks(taskList);
+
+    setChecked(false);
+  };
+
   const missing = getMissingPriorities();
 
   return (
@@ -84,25 +145,93 @@ export default function TodoList() {
         </button>
       </form>
 
+      {/* Errors */}
+      <div className="text-red-600">{error}</div>
+
       {/* Task List */}
+      <div>Tasks</div>
       <ul className="space-y-3 overflow-auto flex-1">
         {tasks.map((task, index) => (
           <li
             key={index}
             className="flex justify-between items-center p-3 bg-gray-50 rounded-xl shadow-sm hover:shadow-md hover:bg-gray-100 transition-all"
           >
+            {edittingMode !== index ? (
+              <>
+                <div className="flex gap-4">
+                  <input
+                    onChange={() => handleComplete(index)}
+                    checked={checked}
+                    type="checkbox"
+                  />
+                  <span className="text-gray-700 font-medium">{task.text}</span>
+                </div>
+                <div className="flex items-center gap-3">
+                  <span className="text-gray-500 text-sm">
+                    Priority: {task.priority}
+                  </span>
+                  <button
+                    onClick={() => goIntoEditting(index, task)}
+                    className="bg-yellow-100 text-yellow-700 hover:bg-yellow-200 hover:text-yellow-800 px-3 py-1 rounded-full text-sm font-medium transition-colors"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => deleteTask(index)}
+                    className="bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-800 px-3 py-1 rounded-full text-sm font-medium transition-colors"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <input type="checkbox" />
+                  <input
+                    value={edittingText}
+                    onChange={(e) => setEdittingText(e.target.value)}
+                    className="text-gray-700 font-medium border p-1"
+                  />
+                </div>
+                <div className="flex items-center gap-3">
+                  <span className="text-gray-500 text-sm">Priority:</span>
+                  <input
+                    value={edittingPriority}
+                    onChange={(e) => setEdittingPriority(e.target.value)}
+                    className="text-gray-700 font-medium border p-1"
+                  />
+                  <button
+                    onClick={() => saveEdit(index)}
+                    className="bg-green-100 text-green-700 hover:bg-green-200 hover:text-green-800 px-3 py-1 rounded-full text-sm font-medium transition-colors"
+                  >
+                    Save
+                  </button>
+                  <button
+                    onClick={() => setEdittingMode(null)}
+                    className="bg-gray-100 text-gray-700 hover:bg-gray-200 hover:text-gray-800 px-3 py-1 rounded-full text-sm font-medium transition-colors"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => deleteTask(index)}
+                    className="bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-800 px-3 py-1 rounded-full text-sm font-medium transition-colors"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </>
+            )}
+          </li>
+        ))}
+
+        <div>Completed tasks</div>
+        {completedTasks.map((task, index) => (
+          <li
+            key={index}
+            className="flex justify-between items-center p-3 bg-gray-50 rounded-xl shadow-sm hover:shadow-md hover:bg-gray-100 transition-all"
+          >
             <span className="text-gray-700 font-medium">{task.text}</span>
-            <div className="flex items-center gap-3">
-              <span className="text-gray-500 text-sm">
-                Priority: {task.priority}
-              </span>
-              <button
-                onClick={() => deleteTask(index)}
-                className="bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-800 px-3 py-1 rounded-full text-sm font-medium transition-colors"
-              >
-                Delete
-              </button>
-            </div>
           </li>
         ))}
       </ul>
